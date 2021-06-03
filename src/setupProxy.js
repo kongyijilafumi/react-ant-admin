c480baf52926e2ceb0e8d76523504c1bb7aca252
孔乙己拉夫米 <1369501150@qq.com>
2021-06-03 16:04:04 +0800

    支持本地代理配置

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,21 +1,24 @@
-const proxy = require('http-proxy-middleware') // 引入即可，不需要安装 
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {  
-  app.use(    
-      // proxy('/api', {  //api1是需要转发的请求(所有带有/api1前缀的请求都会转发给5000)    
-      //    target: 'http://jsonplaceholder.typicode.com', //配置转发目标地址(能返回数据的服务器地址)  
-      //    changeOrigin: true, //控制服务器接收到的请求头中host字段的值  
-      //    /*  
-      //       changeOrigin设置为true时，服务器收到的请求头中的host为：localhost:5000  
-      //       changeOrigin设置为false时，服务器收到的请求头中的host为：localhost:3000  
-      //       changeOrigin默认值为false，但我们一般将changeOrigin值设为true  
-      //    */  
-      //    pathRewrite: {'^/api': ''} //去除请求前缀，保证交给后台服务器的是正常请求地址(必须配置)  
-      // }),  
-      proxy('/api', {   
-        target: 'http://172.21.212.219:666',  
-        changeOrigin: true,  
-        pathRewrite: {'^/api': ''}  
-      })  
-  )  
-}
+const matchUrl = "/api"; // 请求是匹配的地址
+const target = "https://azhengpersonalblog.top/"; // 目标网址
+const prevUrl = "^/api"; // 以/api路径截取
+const writeUlr = "/api/react-ant-admin"; // 重写请求路径  
+/**
+ * 在使用 本地代理转发 请将 src/common/ajax.js axios的config baseURL 置为"/"
+ * 假设本地 ajax 请求以/api开头，将去请求 目标网址 target
+ * ajax.post("/api/getlist") 将/api 重写为 /api/react-ant-admin 
+ * 然后拼接 https://azhengpersonalblog.top/
+ * 如：ajax.post("/api/getlist") => https://azhengpersonalblog.top/api/react-ant-admin/getlist
+ */
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(matchUrl, {
+      target,
+      changeOrigin: true, 
+      secure: true,// https
+      pathRewrite: { [prevUrl]: writeUlr },
+    })
+  );
+};
