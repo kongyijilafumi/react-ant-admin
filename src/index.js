b28b8fe72068ce2f1db85fe63f4bffc122df6f47
孔乙己拉夫米 <1369501150@qq.com>
2021-08-03 17:50:50 +0800

    兼容ie10+

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import 'core-js/es'  
+import 'react-app-polyfill/ie9'  
+import 'react-app-polyfill/stable'
 ReactDOM.render(<App />, document.getElementById("root"));
