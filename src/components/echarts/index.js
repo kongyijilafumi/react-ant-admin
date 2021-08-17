912faf5913d5f6deda90c80f959d63507ccc903c
孔乙己拉夫米 <1369501150@qq.com>
2021-08-17 14:59:02 +0800

    update 1.0

--- src/components/echarts/index.js
+++
@@ -1,6 +0,0 @@
-import loadable from "@loadable/component";
-const Line = loadable(() => import("./line"));
-const Bar = loadable(() => import("./bar"));
-const Pie = loadable(() => import("./pie"));
-
-export { Line, Bar, Pie };
--- src/store/user/action.js
+++
@@ -1,9 +0,0 @@
-import * as ActionTypes from "./actionTypes";
-
-export const setUserInfoAction = (info) => ({
-  type: ActionTypes.SET_USERINFO,
-  info,
-});
-export const clearUser = () => ({
-  type: ActionTypes.CLEAR_USERINFO,
-});
