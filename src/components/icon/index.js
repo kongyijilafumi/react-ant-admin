49d38be63c5715aedec96ec27498655093da4d91
孔乙己拉夫米 <zhangzhengzeng131@qq.com>
2022-05-25 18:24:29 +0800

    iconfont使用本地文件

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from "@ant-design/icons";
 
 const MyIcon = createFromIconfontCN({
-  scriptUrl: "//at.alicdn.com/t/font_2467607_sf5ou36jx9q.js", // 在 iconfont.cn 上生成
+  scriptUrl: process.env.PUBLIC_URL+"/iconfont.js", // 在 iconfont.cn 上生成 "//at.alicdn.com/t/font_2467607_sf5ou36jx9q.js"
 });
 
 export default function Icon({ type, ...itemProps }) {
