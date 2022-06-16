60f106fd3fa44027329718283899d2901af7e609
孔乙己拉夫米 <zhangzhengzeng131@qq.com>
2022-06-16 11:18:52 +0800

    全局样式

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
-
+import "./assets/css/global.less";
 createRoot(document.getElementById('root') as any).render(
   <App />
 )
