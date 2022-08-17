6f41cc821f79276ded38945e99e7919d117a3d82
孔乙己拉夫米 <zhangzhengzeng131@qq.com>
2022-08-17 10:27:56 +0800

    主题色打包优化调整

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { Provider } from "react-redux";
 import store from "./store";
 import loadable from "@loadable/component";
-import Theme from "./components/theme";
 import LayoutSet from "./components/layout-set";
 const AppRouter = loadable(() => import("./router/appRouter"));
+
+function Theme() {
+  if (process.env.showColorSet) {
+    const Com = loadable(() => import("@/components/theme"))
+    return <Com />
+  }
+  return null
+}
+
 function App() {
   return (
     <Provider store={store}>
       <AppRouter />
-      {process.env.showColorSet && <Theme />}
+      <Theme />
       <LayoutSet />
     </Provider>
   );
